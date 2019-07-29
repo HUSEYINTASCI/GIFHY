67e889d660e6bb17e31cff82b621e91c39bfb44d
huseyintasci <huseyintasci@yandex.com>
2019-07-29 00:19:31 -0700

    Added side navigator,creaded api key and ajax

---
+++ assets/javascript/gify.js
@@ -0,0 +1,73 @@
+// Navigation
+
+function openNav() {
+    document.getElementById("gifnav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("gifnav").style.width = "0";
+}
+
+
+//Gifs
+var garry = ["San Francisco", "Apple", "Google", "Facebook"];
+
+//Displaying movie data
+function renderButtons() {
+    $("#buttons-view").empty();
+
+    for (var i = 0; i < garry.length; i++) {
+        createbtn(garry[i]);
+    }
+}
+
+function createbtn(mName) {
+    var b = $("<button>");
+    b.attr("class", "bMovie");
+    b.text(mName);
+    $("#buttons-view").append(b);
+}
+
+$(document).on("click", ".bMovie", function () {
+    $(gifs).empty();
+    var gname = $(this).text();
+
+    var gifapi =
+        "http://api.giphy.com/v1/gifs/search?q=" +
+        gname +
+        "&api_key=wzyt31vpNEDRJvaCFgiyX1EhWGHvzqI6";
+
+    $.ajax({
+        url: gifapi,
+        method: "GET"
+    }).then(function (gif) {
+        var results = gif.data;
+
+        for (var i = 0; i < gif.data.length; i++) {
+            var rating = results[i].rating;
+            var p = $("<p>").text("Rating: " + rating);
+            var image = $("<img>");
+
+            image.attr("src", results[i].images.fixed_height_still.url);
+            image.attr("data-still", results[i].images.fixed_height_still.url);
+            image.attr("data-animate", results[i].images.fixed_height.url);
+            image.attr("data-state", "still");
+            image.addClass("image");
+
+            $("#gifs").append(p);
+            $("#gifs").append(image);
+        }
+    });
+});
+ 
+$("#add-Search").on("click", function () {
+    var sn = $("#Search-input").val();
+
+    garry.push(sn);
+    createbtn(sn);
+
+    event.preventDefault();
+    $("#movie-input").val("");
+});
+
+renderButtons();
